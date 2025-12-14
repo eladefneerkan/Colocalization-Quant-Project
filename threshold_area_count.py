862fde78e64707df5e75d3f19f73c5c9f916a519
Lucia Liu <lucia.liu14@gmail.com>
2025-12-14 00:20:55 -0800

    save output files into folder

--- threshold_area_count.py
+++ threshold_area_count.py
@@ -1,9 +1,15 @@
-# Author: Lucia Liu
-# Purpose: Finds the number of particles and area of each particle for each channel in a .czi file 
-# Last edited: 11/28/25
+from ij import IJ, WindowManager, ImagePlus
+from ij.io import DirectoryChooser, FileSaver
+from ij.measure import ResultsTable
 
-from ij import IJ, WindowManager
+# Choose directory
+dc = DirectoryChooser("Choose folder to save output files in.")
+output_dir = dc.getDirectory()
 
+if output_dir is None:
+    raise ValueError("No output directory selected.")
+
+# Get input image
 imp = IJ.getImage()  # gets dragged-in image
 
 if not imp.getTitle().lower().endswith(".czi"):
@@ -11,20 +17,3 @@ if not imp.getTitle().lower().endswith(".czi"):
 
 IJ.run(imp, "Split Channels", "")
 image_titles = WindowManager.getImageTitles()
-
-for title in image_titles:
-    dup = WindowManager.getImage(title).duplicate()  # create duplicate so we keep the original image
-    dup.show()
-    WindowManager.setCurrentWindow(dup.getWindow())
-
-    # apply threshold
-    IJ.run("8-bit")
-    IJ.run("Auto Threshold", "method=Default dark")
-    IJ.run("Convert to Mask", "method=Default background=Dark")
-
-    # analyze particles
-    IJ.run(dup, "Analyze Particles...", "show=Outlines display summarize exclude")
-        # Outlines = shows image w/ outline and numbered particles
-        # display = makes table of list of areas for each particle in an image
-        # summarize = summarizes data (count, total area, avg size, % area) for each image
-        # exclude = excludes the particles on the edges (since their real size might be bigger/smaller --> not accurate)
